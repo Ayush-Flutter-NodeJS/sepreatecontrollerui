90d57b3942c0ebf1f39114a001202f55a65bb418
Ayush-Flutter-NodeJS <ayush.save14@gmail.com>
2024-10-01 15:27:46 +0530

    first commit

---
+++ lib/mycontroler.dart
@@ -0,0 +1,7 @@
+import 'package:get/get.dart';
+class ayush extends GetxController{
+  var count=0.obs;
+ void incrementcounter(){ // function inside a class.
+    count++;
+  }
+}
---
+++ lib/screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getapi/mycontroler.dart';
+
+class service extends StatefulWidget {
+  const service({super.key});
+
+  @override
+  State<service> createState() => _serviceState();
+}
+
+class _serviceState extends State<service> {
+   ayush Ayush =Get.put(ayush());// creating an instance ot object Ayush is a object
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          ElevatedButton(onPressed: (){
+           Ayush. incrementcounter();
+
+          }, child: Text('hello world')),
+          Obx(()=>Text("${Ayush.count}")), //obx logic
+        ],
+      ),
+    );
+  }
+}
+
+//we can call a class,methods,functions by creating an object of the class.also we can call a varibale by the object.
+//here in this code Ayush is a object 
